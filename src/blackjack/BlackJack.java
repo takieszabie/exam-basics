593294076e831f38882a9732b68a8b3c21d8ad8e
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-18 13:07:39 +0200

    task3 'BlackJack' initial formatting

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -15,13 +15,16 @@ public class BlackJack {
     // We should be able to draw the top card which returns the drawn card and also removes it from the deck
     // Deck should be represented as string in this format:
     // 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
+
     Deck deck = new Deck(12);
     System.out.println(deck);
+
     // Should print out:
     // 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
     Card drawn = deck.draw();
     System.out.println(drawn);
     System.out.println(deck);
+
     // Should print out:
     // Queen Spades
     // 11 cards - 3 Clubs, 3 Diamonds, 3 Hearts, 2 Spades
