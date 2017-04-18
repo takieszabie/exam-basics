9eb9425edb3c7829edb52d4f6f7e804725ce25b9
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-18 10:19:10 +0200

    task1 'OddAvg' implement oddAverage function

--- src/oddavg/OddAvg.java
+++ src/oddavg/OddAvg.java
@@ -1,9 +1,25 @@
 package oddavg;
 
+import java.util.List;
+
 public class OddAvg {
+
   public static void main(String[] args) {
     // Create a function called `oddAverage` that takes a list of numbers as parameter
     // and returns the average value of the odd numbers in the list
     // Create basic unit tests for it with at least 3 different test cases
+  }
+
+  public static double oddAverage(List<Integer> inputNumbers) {
+    int sum = 0;
+    int numberOfItems = 0;
+
+    for (Integer number : inputNumbers) {
+      if (number % 2 == 1) {
+        sum += number;
+        numberOfItems++;
+      }
+    }
+    return (double) sum / (double) numberOfItems;
   }
 }
