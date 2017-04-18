0e53ab6111041acaf0fd7f7970e8a990987902ee
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-18 13:21:52 +0200

    task3 'BlackJack' add initial Deck fill method

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -1,8 +1,21 @@
 package blackjack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
   List<Card> contents;
 
+  public Deck(int numberOfCards) {
+    contents = new ArrayList<>();
+    fill(numberOfCards);
+  }
+
+  private void fill(int cards) {
+    for (int i = 0; i < cards; i++) {
+
+      
+    }
+
+  }
 }
