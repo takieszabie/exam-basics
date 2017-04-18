c397797cfaf42955920268d7e2cbfb180d9cde4c
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-18 13:08:42 +0200

    task3 'BlackJack' initial formatting further

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -10,9 +10,12 @@ public class BlackJack {
     // Jack Diamonds
     // Create a Deck class, that has a list of cards in it
     // Create a constructor that takes a whole number as parameter
-    // The constructor should fill the list with the number of different cards using at least 4 different colors (except if the number given is smaller than four, than all cards should have different colors)
+    // The constructor should fill the list with the number of different cards
+    // using at least 4 different colors (except if the number given is smaller than four,
+    // than all cards should have different colors)
     // We should be able to shuffle the deck, which randomly orders the cards
-    // We should be able to draw the top card which returns the drawn card and also removes it from the deck
+    // We should be able to draw the top card which returns the drawn card and also removes it
+    // from the deck
     // Deck should be represented as string in this format:
     // 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
 
